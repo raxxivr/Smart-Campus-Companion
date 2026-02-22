73c7b0c8f886e274ec7206b0a7182464fbf596f6
Althea <hapathea0304@gmail.com>
2026-02-22 23:56:06 +0800

    Create SettingsScreen composable

---
+++ app/src/main/java/com/example/smartcampuscompanion/ui/screens/SettingsScreen.kt
@@ -0,0 +1,19 @@
+package com.example.smartcampuscompanion.ui.screens
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.example.smartcampuscompanion.ui.viewmodel.SettingsViewModel
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen(
+    username: String?,
+    onLogout: () -> Unit,
+    viewModel: SettingsViewModel,
+    modifier: Modifier = Modifier
+) {
+
+}
